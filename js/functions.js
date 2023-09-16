c401b4dec35901d6a24a0c8d379ee1fe367b016b
Erlend Geerts <erlen@lightning.local>
2023-09-16 10:26:12 +0200

    working getAllCocktails function

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-const getAllCocktails = async () => {
+export const getAllCocktails = async () => {
   // zoek naar de api endpoint dat alle cocktails opvraagt beginnend met de letter L
   // thecocktaildb
   // fetch met .then.catch
@@ -10,16 +10,41 @@ const getAllCocktails = async () => {
   //     .then((response) => response.json())
   //     .then((data) => console.log(data.drinks.map((drink) => drink.strDrink)))
   //     .catch((err) => console.log(err));
+  //   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alphabet = new Array(26)
+    .fill(65)
+    .map((el, i) => String.fromCharCode(el + i));
 
-  const allDownloads = [
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=l"),
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=b"),
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=d"),
-  ];
+  const allDownloads = alphabet.map((letter) =>
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
+  );
+
+  //   const allDownloads = [
+  //     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=l"),
+  //     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=b"),
+  //     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=x"),
+  //   ];
+  console.log(allDownloads);
   const allResponses = await Promise.all(allDownloads);
-  const allDatas = await Promise.all(allResponses.map((r) => r.json()));
-  const allDrinks = allDatas.map((d) => d.drinks);
+  console.log(allResponses);
+  const allDatas = await Promise.all(
+    allResponses.map((response) => response.json())
+  );
+  console.log(allDatas);
+  const allDrinks = allDatas
+    .filter((data) => data.drinks != null)
+    .map((data) => data.drinks);
 
-  console.log(allDrinks.flat().map((d) => d.strDrink));
+  //   console.log(allDrinks.flat().map((data) => ({
+  //     name: data.strDrink,
+  //     id: data.idDrink,
+  //     photo: data.strDrinkThumb
+  //   })));
+
+  return allDrinks.flat().map(({ strDrink, idDrink, strDrinkThumb }) => ({
+    // ronde haakje nodig want anders ziet hij het als een function ipv van object
+    strDrink,
+    idDrink,
+    strDrinkThumb,
+  }));
 };
-getAllCocktails();
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 import "/css/style.scss";
+import { getAllCocktails } from "./functions.js";
+
+async function main() {
+  const allCocktails = await getAllCocktails();
+  console.log(allCocktails);
+}
+
+main();
